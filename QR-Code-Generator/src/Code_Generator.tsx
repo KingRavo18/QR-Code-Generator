43bc80fc107ba5301ca9e101a3a4241b2e03e33d
KingRavo18 <rkingovskis14@gmail.com>
2026-01-01 19:29:16 +0200

    d

--- QR-Code-Generator/src/Code_Generator.tsx
+++ QR-Code-Generator/src/Code_Generator.tsx
@@ -1,9 +1,33 @@
-import type { JSX } from "react";
+import { useState, type JSX } from "react";
 
 export default function QrCodeGenerator(): JSX.Element{
+    const [link, setLink] = useState<string>("");
+
+    function generateCode(): void{
+
+    }
+
     return(
         <main>
+            <h1>QR Code Generator</h1>
+            <div>
+                <input type="url"
+                       placeholder="Enter link..."
+                       title="Enter the link you want a QR code for"
+                       aria-label="Enter the link you want a QR code for"
+                       onChange={event => setLink(event.target.value)}
+                />
+                <button onClick={generateCode}
+                        title="Generate your QR code"
+                        aria-label="Generate your QR code"
+                >
+                    Generate
+                </button>
+            </div>
+            <div>
 
+            </div>
+            <button>Download QR Code</button>
         </main>
     )
 }
